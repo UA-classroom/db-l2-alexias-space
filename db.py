55542880c9211aa4b7a8b76c208c456aa5483998
Alexia <alexiakazim99@gmail.com>
2025-12-13 15:51:26 +0100

    get user by id function inserted

--- db.py
+++ db.py
@@ -9,3 +9,13 @@ def get_all_users(con):
         with con.cursor(cursor_factory=RealDictCursor) as cursor:
             cursor.execute("SELECT * FROM users;")
             return cursor.fetchall() 
+        
+def get_user_by_id(con, user_id):
+    """Fetch a specific user"""
+    with con:
+        with con.cursor(cursor_factory=RealDictCursor) as cursor:
+            cursor.execute("SELECT * FROM users WHERE user_id = %s;", (user_id,))
+            user = cursor.fetchone()
+            if not user:
+                raise Exception(f"User with id {user_id} not found")
+            return user

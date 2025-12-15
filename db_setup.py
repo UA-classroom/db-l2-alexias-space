fc95e22b7c1f2d7b8994135cd5515a1a6e914c92
Alexia <alexiakazim99@gmail.com>
2025-12-15 20:05:59 +0100

    all tabels created

--- db_setup.py
+++ db_setup.py
@@ -26,12 +26,101 @@ def get_connection():
 
 
 def create_tables():
-    """
-    A function to create the necessary tables for the project.
-    """
-    connection = get_connection()
-    # Implement
-    pass
+    
+    #Users table 
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            user_id SERIAL PRIMARY KEY, 
+            first_name VARCHAR(100) NOT NULL,
+            last_name VARCHAR(100) NOT NULL,
+            email VARCHAR(255) UNIQUE NOT NULL, 
+            phone_number VARCHAR(100) NOT NULL,
+            password_hash VARCHAR(255) NOT NULL,
+            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP 
+        );
+    """)
+    
+    # salons table
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS salons (
+            salon_id SERIAL PRIMARY KEY, 
+            owner_id INTEGER NOT NULL REFERENCES users(user_id),
+            name VARCHAR(255) UNIQUE NOT NULL,
+            adress VARCHAR(255) NOT NULL, 
+            city VARCHAR(100) NOT NULL, 
+            postal_code VARCHAR(10) NOT NULL, 
+            phone_number VARCHAR(20) NOT NULL,
+            email VARCHAR(255) UNIQUE NOT NULL,
+            description TEXT, 
+            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+        );
+    """)
+    
+    # services table
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS services (
+            service_id SERIAL PRIMARY KEY,
+            salon_id INTEGER REFERENCES salons(salon_id) NOT NULL,
+            name VARCHAR(150) NOT NULL,
+            description TEXT NOT NULL, 
+            price DECIMAL(10,2) NOT NULL,
+            is_active BOOLEAN DEFAULT true,
+            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+        );
+    """)
+    
+    #bookings table
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS bookings (
+            booking_id SERIAL PRIMARY KEY,
+            user_id INTEGER REFERENCES users(user_id) NOT NULL,
+            salon_id INTEGER REFERENCES salons(salon_id) NOT NULL,
+            service_id INTEGER REFERENCES services(service_id) NOT NULL,
+            start_time TIMESTAMP WITH TIME ZONE NOT NULL, 
+            end_time TIMESTAMP WITH TIME ZONE NOT NULL, 
+            status VARCHAR(50) NOT NULL
+        );
+    """)
+    
+    #payments table
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS payments (
+            payment_id SERIAL PRIMARY KEY,
+            booking_id INTEGER REFERENCES bookings(booking_id) NOT NULL,
+            user_id INTEGER REFERENCES users(user_id) NOT NULL,
+            amount DECIMAL(10, 2) NOT NULL,
+            payment_method VARCHAR(50) NOT NULL,
+            transaction_status VARCHAR(50) NOT NULL,
+            payment_date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+        );
+    """)
+    
+    # Rewiew table 
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS reviews (
+            review_id SERIAL PRIMARY KEY,
+            salon_id INTEGER REFERENCES salons(salon_id) NOT NULL, 
+            user_id INTEGER REFERENCES users(user_id) NOT NULL, 
+            rating INTEGER CHECK (rating >= 1 AND rating <= 5) NOT NULL,
+            comment TEXT,
+            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+        );
+    """)
+    
+    
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
